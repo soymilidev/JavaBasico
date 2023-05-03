d93e25db5137c8672cd18dab2ecee92f4a4d8865
soymilidev <milicastro.dev@gmail.com>
2023-05-02 21:59:47 -0300

    Actualizacion de archivo E1

---
+++ Entrega01/src/com/company/ResolucionEjercicio.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class ResolucionEjercicio {
+    public static void main(String[] args) {
+        // enteros
+        byte number1 = 1; // 1 byte
+        short number2 = 10; // 2 bytes
+        int number3 = 50; // 4 bytes
+        long number4 = 100; // 8 bytes
+
+        System.out.println("Numeros enteros: " + number1 + " , " + number2 + " , " + number3 + " , " + number4);
+
+        // decimales
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+
+        System.out.println("Decimales: " + decimal1 + " , " + decimal2);
+
+        // cadena de texto
+        String nombre = "Jose David";
+        String apellido = "Prieto";
+
+        // caracter
+        char caracter1 = 'a';
+
+        System.out.println("Cadena de texto: " + nombre + " " + apellido + "\nCaracter: " + caracter1);
+
+        //booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+    }
+}
+
+// Fuente: https://github.com/Open-Bootcamp/ResolucionEjercicios/blob/main/Java%20B%C3%A1sico/Ejercicio%20Sesi%C3%B3n%201/ejercicio1.java
