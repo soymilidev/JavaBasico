403242908b8a0bf09854abba2a8ca3801414ce84
soymilidev <milicastro.dev@gmail.com>
2023-05-02 21:48:51 -0300

    Creacion de archivo E1

---
+++ Entrega01/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        byte peso = 127;
+        short distancia = 32767;
+        int edad = 30;
+        int numeroPrimo = 2147483647;
+        long codigo = 123456789012345L;
+
+        //tipos de datos de punto flotante
+        float precio = 0.123456789f;
+        double toneladas = 0.123456789;
+
+        boolean respuesta = true;
+        String nombre = "Sol";
+        char inicial = 'a';
+
+        System.out.println(peso);
+        System.out.println(distancia);
+        System.out.println(edad);
+        System.out.println(numeroPrimo);
+        System.out.println(codigo);
+
+        System.out.println(precio);
+        System.out.println(toneladas);
+
+        System.out.println(respuesta);
+        System.out.println(nombre);
+        System.out.println(inicial);
+    }
+}
